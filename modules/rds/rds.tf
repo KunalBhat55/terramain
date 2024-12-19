995f86e72f26d005020fd952d6ee041cc1aa53cc
KunalBhat55 <kunalbhat6767@gmail.com>
2024-12-19 12:15:12 +0530

    Remove hardcoded database username and password from RDS module

--- modules/rds/rds.tf
+++ modules/rds/rds.tf
@@ -1,8 +1,6 @@
 resource "aws_db_instance" "main-db" {
   db_name             = "maindb"
   instance_class      = "db.t3.micro"
-  username            = "kunal"
-  password            = "asdfghjkl"
   allocated_storage   = 20
   engine              = "postgres"
   publicly_accessible = true
